494b3834e80c5f1252a790ddf9dc402671cfd889
Aljohara <50531996+aljohara177@users.noreply.github.com>
2020-11-28 17:11:36 +0300

    Fix webpack prod rules

--- webpack.prod.js
+++ webpack.prod.js
@@ -19,7 +19,7 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.scss$/,
+                test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ]
